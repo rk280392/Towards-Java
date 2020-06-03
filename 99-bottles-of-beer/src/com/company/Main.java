cf953a3dc72bdc69ead0042fa7fae6b68066e8f5
Rajesh Kumar <rk90229@gmail.com>
2020-06-03 20:02:22 +0530

    added \n

--- 99-bottles-of-beer/src/com/company/Main.java
+++ 99-bottles-of-beer/src/com/company/Main.java
@@ -15,19 +15,19 @@ public class Main {
         if (bottleNum == 0) {
             System.out.print("No more " + word + "s" + " of beer on the wall,");
             System.out.println("no more " + word + "s" + " of beer");
-            System.out.println("Go to the store and buy some more," + " 99 bottles of beer on the wall");
+            System.out.println("Go to the store and buy some more," + " 99 bottles of beer on the wall.\n");
         } else if (bottleNum == 1) {
             System.out.print(bottleNum + " " + word + " of beer on the wall,");
             System.out.println(bottleNum + " " + word + " of beer.");
-            System.out.println("Take one down and pass it around, no more " + word + "s" + " of beer on the wall.");
+            System.out.println("Take one down and pass it around, no more " + word + "s" + " of beer on the wall.\n");
         } else if (bottleNum == 2) {
             System.out.print(bottleNum + " " + word + "s" + " of beer on the wall,");
             System.out.println(bottleNum + " " + word + "s" + " of beer.");
-            System.out.println("Take one down and pass it around," + (bottleNum - 1) + " " + word + " of beer on the wall.");
+            System.out.println("Take one down and pass it around," + (bottleNum - 1) + " " + word + " of beer on the wall.\n");
         } else {
             System.out.print(bottleNum + " " + word + "s" + " " + " of beer on the wall,");
             System.out.println(bottleNum + " " + word + "s" + " " + "of beer.");
-            System.out.println("Take one down and pass it around, " + (bottleNum - 1) + " " + word + "s" + " of beer on the wall.");
+            System.out.println("Take one down and pass it around, " + (bottleNum - 1) + " " + word + "s" + " of beer on the wall.\n");
         }
     }
 }
