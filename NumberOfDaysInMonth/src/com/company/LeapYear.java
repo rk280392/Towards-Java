b1cb39ff821d08647fa210da211f461c17ff9de6
Rajesh Kumar <rk90229@gmail.com>
2020-06-07 07:59:05 +0530

    fixed bug in leap year.java

--- NumberOfDaysInMonth/src/com/company/LeapYear.java
+++ NumberOfDaysInMonth/src/com/company/LeapYear.java
@@ -7,7 +7,7 @@ public class LeapYear {
         }
         if (year % 4 == 0 ) {
             if (year % 100 == 0) {
-                if (year % 400 == 9) {
+                if (year % 400 == 0) {
                     return true;
                 }else {
                     return false;
