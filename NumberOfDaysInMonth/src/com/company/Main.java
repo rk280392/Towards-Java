7ce1d5741143137884addc66d8877645246e64da
Rajesh Kumar <rk90229@gmail.com>
2020-06-07 07:30:44 +0530

    more practice test

---
+++ NumberOfDaysInMonth/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       int days = NumberOfDaysInMonth.getDaysInMonth(2, -2020);
+        System.out.println(days);
+    }
+}
